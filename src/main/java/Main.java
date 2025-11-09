4157102d1c66a0e2bb5dd7685d5fc21f29c4ee7c
phonty29 <amedov.bekmuhamet@gmail.com>
2025-11-09 19:59:00 +0500

    feat: print table names

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
   public static void main(String[] args){
@@ -23,6 +25,7 @@ public class Main {
           databaseFile.read(pageSizeBytes);
           short pageSizeSigned = ByteBuffer.wrap(pageSizeBytes).getShort();
           int pageSize = Short.toUnsignedInt(pageSizeSigned);
+          // Skip until b-tree page header
           databaseFile.skip(100 - 18 + 3);
 
           byte[] numberOfTablesBytes = new byte[2];
@@ -37,6 +40,48 @@ public class Main {
         } catch (IOException e) {
           System.out.println("Error reading file: " + e.getMessage());
         }
+      }
+      case ".tables" -> {
+        try {
+          FileInputStream databaseFile = new FileInputStream(new File(databaseFilePath));
+
+          databaseFile.skip(103); // Skip the first 16 bytes of the header
+          byte[] numberOfTablesBytes = new byte[2];
+          databaseFile.read(numberOfTablesBytes);
+          short numberOfTables = ByteBuffer.wrap(numberOfTablesBytes).getShort();
+          // Skip until cell pointer array
+          databaseFile.skip(3);
+          byte[] cellPointerArrayBytes = new byte[2*numberOfTables];
+          databaseFile.read(cellPointerArrayBytes);
+
+          ByteBuffer cellPointerArray = ByteBuffer.wrap(cellPointerArrayBytes);
+          short[] tableOffsets = new short[numberOfTables];
+          for (int i = 0; i < numberOfTables; i++) {
+            // The last table offset goes first, so printing order from last-to-first
+            tableOffsets[i] = cellPointerArray.getShort();
+          }
+
+          int lastTableOffset = tableOffsets[0];
+          int firstTableOffset = tableOffsets[numberOfTables-1];
+          int tableDataSize = lastTableOffset - firstTableOffset;
+          int skipToFirstOffset = firstTableOffset - (108 + (2*numberOfTables));
+          databaseFile.skip(skipToFirstOffset);
+
+          byte[] tableData = new byte[tableDataSize];
+          databaseFile.read(tableData);
+
+          String[] tableNames = Utils.getTableNamesFromRecords(tableData, tableOffsets.length);
+          tableNames[tableNames.length-1] = Utils.getLastTableNameFromFile(databaseFile);
+
+          Collections.reverse(Arrays.asList(tableNames));
+          // You can use print statements as follows for debugging, they'll be visible when running tests.
+          System.err.println("Logs from your program will appear here!");
+
+          System.out.println(String.join(" ", tableNames));
+        } catch (IOException e) {
+          System.out.println("Error reading file: " + e.getMessage());
+        }
+
       }
       default -> System.out.println("Missing or invalid command passed: " + command);
     }
---
+++ src/main/java/Utils.java
@@ -0,0 +1,91 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class Utils {
+  public static String getLastTableNameFromFile(FileInputStream fileInputStream)
+      throws IOException {
+    int payloadSize = fileInputStream.read();
+    // Skip rowid
+    fileInputStream.read();
+    // Payload starts here
+    int payloadHeaderSize = fileInputStream.read();
+    int typeSize = getSize(fileInputStream.read());
+    int nameSize = getSize(fileInputStream.read());
+    int tableNameSize = getSize(fileInputStream.read());
+    // Skip rest record header
+    fileInputStream.read(new byte[payloadHeaderSize - 4]);
+    // Record body starts here
+    // Skip sqlite_schema.type from body
+    byte[] typeBytes = new byte[typeSize];
+    fileInputStream.read(typeBytes);
+    // Skip sqlite_schema.name
+    byte[] nameBytes = new byte[nameSize];
+    fileInputStream.read(nameBytes);
+    // Get table name sqlite_schema.tbl_name
+    byte[] tableNameBytes = new byte[tableNameSize];
+    fileInputStream.read(tableNameBytes);
+    return new String(tableNameBytes);
+  }
+
+  public static String[] getTableNamesFromRecords(byte[] records, int length) {
+    String[] tableNames = new String[length];
+    ByteBuffer recordBuffer = ByteBuffer.wrap(records);
+
+    int i = 0;
+    int offset = 0;
+    while (recordBuffer.hasRemaining() && i < length) {
+      byte payloadSize = recordBuffer.get();
+      // Skip rowid
+      recordBuffer.get();
+      // Payload starts here
+      byte payloadHeaderSize = recordBuffer.get();
+      byte typeSize = getSize(recordBuffer.get());
+      byte nameSize = getSize(recordBuffer.get());
+      byte tableNameSize = getSize(recordBuffer.get());
+      // Skip rest record header
+      recordBuffer.get(new byte[payloadHeaderSize - 4]);
+      // Record body starts here
+      // Skip sqlite_schema.type from body
+      byte[] typeBytes = new byte[typeSize];
+      recordBuffer.get(typeBytes);
+      // Skip sqlite_schema.name
+      byte[] nameBytes = new byte[nameSize];
+      recordBuffer.get(nameBytes);
+      // Get table name sqlite_schema.tbl_name
+      byte[] tableNameBytes = new byte[tableNameSize];
+      recordBuffer.get(tableNameBytes);
+      tableNames[i++] = new String(tableNameBytes);
+      // Skip the rest of the record body
+      int skipSize = payloadSize + 2 - (recordBuffer.position() - offset);
+      recordBuffer.get(new byte[skipSize]);
+      offset = recordBuffer.position();
+    }
+    return tableNames;
+  }
+
+  private static int getSize(int serialType) {
+    if (isOddAndBiggerThan13(serialType)) {
+      return (byte) ((serialType - 13) / 2);
+    } else {
+      return (byte) ((serialType - 12) / 2);
+    }
+  }
+
+  private static byte getSize(byte serialType) {
+    if (isOddAndBiggerThan13(serialType)) {
+      return (byte) ((serialType - 13) / 2);
+    } else {
+      return (byte) ((serialType - 12) / 2);
+    }
+  }
+
+  private static boolean isOddAndBiggerThan13(byte b) {
+    return b > 13 && (b%2 == 0);
+  }
+
+  private static boolean isOddAndBiggerThan13(int b) {
+    return b > 13 && (b%2 == 0);
+  }
+}
