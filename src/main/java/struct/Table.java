8cc15a6dba56982fcf475dc4834842ad5f83d352
phonty29 <amedov.bekmuhamet@gmail.com>
2025-12-09 14:32:10 +0500

    fix: handle if rootpage bytes length equal to 3

--- src/main/java/struct/Table.java
+++ src/main/java/struct/Table.java
@@ -180,6 +180,9 @@ public class Table {
     return switch (rootPageBytes.length) {
       case 1 -> Byte.toUnsignedInt(ByteBuffer.wrap(rootPageBytes).get());
       case 2 -> Short.toUnsignedInt(ByteBuffer.wrap(rootPageBytes).getShort());
+      case 3 -> ((rootPageBytes[0] & 0xFF) << 16) |
+          ((rootPageBytes[1] & 0xFF) << 8) |
+          (rootPageBytes[2] & 0xFF);
       case 4 -> ByteBuffer.wrap(rootPageBytes).getInt();
       default -> throw new IllegalStateException("Rootpage couldn't be cast to integer type");
     };
